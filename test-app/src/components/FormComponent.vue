b3f7b8968c23b4b61a7c5a48703b74317c4b3081
liteJoseStalin <79840097+liteJoseStalin@users.noreply.github.com>
2021-11-09 23:08:18 +0100

    creating the form

---
+++ test-app/src/components/FormComponent.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <h1>Add a Contact:</h1>
+    <v-container>
+      <v-row>
+        <v-text-field
+          v-model="prename"
+          :rules="defRules"
+          label="Prename"
+          required
+        ></v-text-field>
+      </v-row>
+      <v-row>
+        <v-text-field
+          v-model="surname"
+          :rules="defRules"
+          label="Surname"
+          required
+        ></v-text-field>
+      </v-row>
+
+      <v-row>
+        <v-col cols="9">
+          <v-text-field
+            v-model="street"
+            :rules="defRules"
+            label="Street"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="3">
+          <v-text-field
+            v-model="number"
+            :rules="defRules"
+            label="Number"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3">
+          <v-text-field
+            v-model="postalCode"
+            :rules="defRules"
+            label="Postal Code"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="9">
+          <v-text-field
+            v-model="city"
+            :rules="defRules"
+            label="City"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+        ></v-text-field>
+      </v-row>
+
+      <v-checkbox v-model="checkbox" label="Recieve Newsletter?"></v-checkbox>
+
+      <v-btn color="error" class="mr-4" @click="reset">Clear Form </v-btn>
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate"
+        >Save Content</v-btn
+      >
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    name: "",
+    defRules: [(v) => !!v || "Name is required"],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    checkbox: false,
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
+};
+</script>
--- test-app/src/plugins/vuetify.js
+++ test-app/src/plugins/vuetify.js
@@ -4,4 +4,9 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme:{
+        primary: '#febf7c', 
+        success: '#24bf77', 
+        error: '#f90000'
+    }
 });
